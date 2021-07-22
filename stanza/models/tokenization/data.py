8c7e874f0603d436f445e6d7b76387456fceea71
vythaihn <vythai@stanford.edu>
2021-07-22 15:59:29 -0700

    new

--- stanza/models/tokenization/data.py
+++ stanza/models/tokenization/data.py
@@ -246,7 +246,7 @@ class DataLoader:
                 if len(current) <= self.args['max_seqlen']:
                     # get rid of sentences that are too long during training of the tokenizer
                     res.append(process_sentence(current))
-                #print(current)
+                print(current)
                 current = []
                 
         if len(current) > 0:
